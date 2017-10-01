90fc1dc69efb453a52693dc058e74365e1070532
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-10-01 14:00:47 -0400

    Redirect to show after create user

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,6 +7,15 @@ defmodule CoffeeRoulettePhx.UserController do
     render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.first_name} created!")
+    |> redirect(to: user_path(conn, :show, user))
+  end
+
   def show(conn, %{"id" => id}) do
     user = Repo.get(CoffeeRoulettePhx.User, id)
     render conn, "show.html", user: user

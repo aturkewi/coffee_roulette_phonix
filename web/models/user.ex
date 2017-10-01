f44e1e589a719c15538fc9ad850bfc65cbb6caf3
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-10-01 14:18:22 -0400

    Add auth to user registration

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,7 +1,7 @@
 defmodule CoffeeRoulettePhx.User do
   use CoffeeRoulettePhx.Web, :model
 
-  @accetable_params ~w(first_name last_name email username password)
+  @accetable_params ~w(first_name last_name email username)
 
   schema "users" do
     field :first_name, :string
@@ -18,5 +18,22 @@ defmodule CoffeeRoulettePhx.User do
     model
     |> cast(params, @accetable_params, [])
     |> validate_length(:username, min: 6, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end

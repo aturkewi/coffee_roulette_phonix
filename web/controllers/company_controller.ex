45dc3ee16f24fefa5341988ebb162a300d84783a
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-12-09 16:54:39 -0500

    Copany show page

--- web/controllers/company_controller.ex
+++ web/controllers/company_controller.ex
@@ -8,26 +8,24 @@ defmodule CoffeeRoulettePhx.CompanyController do
     render conn, "new.html", changeset: changeset
   end
 
-  # def create(conn, %{"user" => user_params}) do
-  #   changeset = User.registration_changeset(%User{}, user_params)
-  #   case Repo.insert(changeset) do
-  #     {:ok, user} ->
-  #       conn
-  #       |> CoffeeRoulettePhx.Auth.login(user)
-  #       |> put_flash(:info, "#{user.first_name} created!")
-  #       |> redirect(to: user_path(conn, :show, user))
-  #     {:error, changeset} ->
-  #       render(conn, "new.html", changeset: changeset)
-  #   end
-  # end
+  def create(conn, %{"company" => company_params}) do
+    changeset = Company.changeset(%Company{}, company_params)
+    case Repo.insert(changeset) do
+      {:ok, company} ->
+        conn
+        |> put_flash(:info, "#{company.name} created!")
+        |> redirect(to: company_path(conn, :show, company))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 
-  # def show(conn, %{"id" => id}) do
-  #   user = User
-  #     |> preload([:companies])
-  #     |> Repo.get(id)
-  #
-  #   render conn, "show.html", user: user
-  # end
+  def show(conn, %{"id" => id}) do
+    company = Company
+      |> Repo.get(id)
+
+    render conn, "show.html", company: company
+  end
 
   defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
